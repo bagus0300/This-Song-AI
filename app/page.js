dfbf9a42e14d8b0ac18c257e9bdad79c6a7a0945
marvin9830 <asamieto965@gmail.com>
2023-10-28 17:15:26 -0400

    Refactored backend link

--- app/page.js
+++ app/page.js
@@ -15,7 +15,7 @@ export default function Home() {
     <main className="flex flex-col items-center justify-between min-h-screen p-24">
       <h1>This Song</h1>
       <section>
-        <a className="App-link" href="http://localhost:8888/login">
+        <a className="App-link" href="http://localhost:8000/login">
           Log in to Spotify
         </a>
       </section>
