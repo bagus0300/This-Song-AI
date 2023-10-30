1542437eb21f937ba89683e0acb85405cd252b1e
marvin9830 <asamieto965@gmail.com>
2023-10-30 16:45:18 -0400

    Installed Shadcn UI

---
+++ app/page.jsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+
+// import { accessToken, logout } from "@/lib/spotify";
+// import { useEffect, useState } from "react";
+
+export default function Home() {
+  // const [token, setToken] = useState(null);
+
+  // useEffect(() => {
+  //   setToken(accessToken);
+  // }, []);
+
+  return (
+    <main className="flex flex-col items-center min-h-screen p-24 justify-evenly">
+      <h1>This Song</h1>
+      <section>
+        {/* <a className="App-link" href="http://localhost:8000/login">
+          Log in to Spotify
+        </a> */}
+        <p>page.jsx</p>
+      </section>
+    </main>
+  );
+}
---
+++ components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <>
+      <main>
+        {/* Navbar */}
+        <nav>Navbar</nav>
+      </main>
+    </>
+  );
+};
+
+export default Header;
