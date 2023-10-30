4a6784631a7477b3724a757043a1378d23939d96
marvin9830 <asamieto965@gmail.com>
2023-10-30 16:55:09 -0400

    Added Button

--- components/Header.jsx
+++ components/Header.jsx
@@ -5,7 +5,12 @@ const Header = () => {
     <>
       <main>
         {/* Navbar */}
-        <nav>Navbar</nav>
+        <nav>
+          <section className="flex flex-row gap-8 p-2">
+            <h1>Navbar</h1>
+            <span>Nav element</span>
+          </section>
+        </nav>
       </main>
     </>
   );
