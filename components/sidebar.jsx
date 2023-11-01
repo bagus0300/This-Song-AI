8b3cf1e1da079725421fedf0f35706c276e8141b
marvin9830 <asamieto965@gmail.com>
2023-11-01 18:45:10 -0400

    Created menu toggle button

--- components/sidebar.jsx
+++ components/sidebar.jsx
@@ -1,22 +1,25 @@
 import React from "react";
 import { ModeToggle } from "./ui/toggle-mode";
 import Link from "next/link";
+import { MenuToggle } from "./ui/toggle-menu";
 
 const Sidebar = () => {
   return (
     <div className="flex flex-col h-full px-3 py-4 md:px-2">
-      <Link
-        className="flex items-end justify-start h-20 p-4 mb-2 bg-blue-600 rounded-md md:h-40"
-        href="/"
-      >
-        <div className="w-32 text-white md:w-40">This Song</div>
-      </Link>
+      <div className="flex items-center justify-between p-4 mb-2 rounded-md bg-zinc-600 h-14">
+        <Link href="/">
+          <div className="text-white">This Song</div>
+        </Link>
+        <section id="toggle-buttons" className="flex gap-2">
+          <ModeToggle />
+          <MenuToggle />
+        </section>
+      </div>
       <div className="flex flex-row justify-between space-x-2 grow md:flex-col md:space-x-0 md:space-y-2">
         {/* <NavLinks /> */}
         <div className="hidden w-full h-auto rounded-md grow bg-gray-50 md:block"></div>
         {/* <form>
           <button className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
-            <PowerIcon className="w-6" />
             <div className="hidden md:block">Sign Out</div>
           </button>
         </form> */}
---
+++ components/ui/toggle-menu.jsx
@@ -0,0 +1,38 @@
+"use client";
+
+import * as React from "react";
+import { useState } from "react";
+import { Menu, X } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import clsx from "clsx";
+
+export function MenuToggle() {
+  const [toggle, setToggle] = useState(false);
+
+  return (
+    <Button
+      variant="outline"
+      size="icon"
+      className="md:hidden"
+      onClick={() => {
+        setToggle(!toggle);
+        console.log("toggle", toggle);
+      }}
+    >
+      <Menu
+        className={clsx(
+          "h-[1.2rem] w-[1.2rem] transition-all",
+          toggle && "rotate-90 scale-0"
+        )}
+      />
+      <X
+        className={clsx(
+          "absolute h-[1.2rem] w-[1.2rem] transition-all",
+          !toggle && "rotate-90 scale-0"
+        )}
+      />
+      <span className="sr-only">Toggle theme</span>
+    </Button>
+  );
+}
