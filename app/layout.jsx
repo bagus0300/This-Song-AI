c01c28bdc1e5c46f4b17c86aabc83bcbdb10c552
marvin9830 <asamieto965@gmail.com>
2023-11-02 23:14:26 -0400

    Removed NextAuth

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,8 +2,6 @@ import "./globals.css";
 import { inter } from "@/components/ui/fonts";
 import Sidebar from "@/components/ui/sidebar";
 import { ThemeProvider } from "@/components/theme-provider";
-import { SessionProvider } from "@/components/session-provider";
-// import { SessionProvider } from "next-auth/react";
 
 export const metadata = {
   title: "This Song",
@@ -16,16 +14,14 @@ export default function RootLayout({ session, children }) {
     <html lang="en" suppressHydrationWarning>
       <body className={`${inter.className} antialiased overflow-hidden`}>
         <ThemeProvider attribute="class" defaultTheme="system">
-          <SessionProvider session={session}>
-            <div className="flex flex-col h-screen md:flex-row md:overflow-hidden">
-              <div className="flex-none w-full md:w-64">
-                <Sidebar />
-              </div>
-              <div className="flex-grow px-4 overflow-y-auto md:p-2">
-                {children}
-              </div>
+          <div className="flex flex-col h-screen md:flex-row md:overflow-hidden">
+            <div className="flex-none w-full md:w-64">
+              <Sidebar />
             </div>
-          </SessionProvider>
+            <div className="flex-grow px-4 overflow-y-auto md:p-2">
+              {children}
+            </div>
+          </div>
         </ThemeProvider>
       </body>
     </html>
--- components/user-profile.jsx
+++ components/user-profile.jsx
@@ -1,7 +1,6 @@
 "use client";
 import React from "react";
 import LoginButton from "./ui/login-button";
-import { useSession, signIn, signOut } from "next-auth/react";
 import Image from "next/image";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import {
@@ -15,38 +14,38 @@ import {
 import { Button } from "./ui/button";
 
 const UserProfile = () => {
-  const { data: session, status } = useSession();
-  const userEmail = session?.user?.email;
+  // const { data: session, status } = useSession();
+  // const userEmail = session?.user?.email;
 
-  if (status === "loading") {
-    return <button>Loading...</button>;
-  }
+  // if (status === "loading") {
+  //   return <button>Loading...</button>;
+  // }
 
-  if (status === "authenticated") {
-    console.log(session);
+  // if (status === "authenticated") {
+  //   console.log(session);
 
-    return (
-      <DropdownMenu>
-        <DropdownMenuTrigger>
-          <div className="flex items-center justify-center w-full gap-2 p-1 text-sm font-medium text-center align-middle rounded-sm bg-primary text-primary-foreground hover:bg-primary/90">
-            <Avatar>
-              <AvatarImage src={session.user.image} />
-              <AvatarFallback>{session.user.name[0]}</AvatarFallback>
-            </Avatar>
-            Signed in as {session.user.name}
-          </div>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent>
-          <DropdownMenuLabel>{userEmail}</DropdownMenuLabel>
-          <DropdownMenuSeparator />
-          <DropdownMenuItem onClick={() => signOut()}>
-            Sign out
-          </DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
-      // {/* <button onClick={() => signOut()}>Sign out</button> */}
-    );
-  }
+  //   return (
+  //     <DropdownMenu>
+  //       <DropdownMenuTrigger>
+  //         <div className="flex items-center justify-center w-full gap-2 p-1 text-sm font-medium text-center align-middle rounded-sm bg-primary text-primary-foreground hover:bg-primary/90">
+  //           <Avatar>
+  //             <AvatarImage src={session.user.image} />
+  //             <AvatarFallback>{session.user.name[0]}</AvatarFallback>
+  //           </Avatar>
+  //           Signed in as {session.user.name}
+  //         </div>
+  //       </DropdownMenuTrigger>
+  //       <DropdownMenuContent>
+  //         <DropdownMenuLabel>{userEmail}</DropdownMenuLabel>
+  //         <DropdownMenuSeparator />
+  //         <DropdownMenuItem onClick={() => signOut()}>
+  //           Sign out
+  //         </DropdownMenuItem>
+  //       </DropdownMenuContent>
+  //     </DropdownMenu>
+  //     // {/* <button onClick={() => signOut()}>Sign out</button> */}
+  //   );
+  // }
 
   return (
     <>
