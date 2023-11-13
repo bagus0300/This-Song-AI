82539adaedb173cbeb2edc8967dacc2ae40d685a
marvin9830 <asamieto965@gmail.com>
2023-11-12 23:55:55 -0500

    Removed old sign-out button

--- components/ui/sidebar.jsx
+++ components/ui/sidebar.jsx
@@ -52,11 +52,6 @@ const Sidebar = () => {
             {/* </Link> */}
           </div>
         </div>
-        {/* <form>
-          <button className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
-            <div className="hidden md:block">Sign Out</div>
-          </button>
-        </form> */}
       </div>
     </div>
   );
