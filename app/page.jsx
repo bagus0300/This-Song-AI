397e3f9c60af7ba07d9dcc0b6ab87775c9bb769d
marvin9830 <asamieto965@gmail.com>
2023-11-18 17:00:55 -0500

    Removed apostrophe

--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Page = () => {
   return (
     <div className="text-3xl">
-      Log in with Spotify to see the song you're currently listening to.
+      Log in with Spotify to see the song you are currently listening to.
     </div>
   );
 };
