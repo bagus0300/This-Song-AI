a7726d1c0d7a9514e6d819c704c5aff9f20d0ccb
marvin9830 <asamieto965@gmail.com>
2023-11-18 16:59:52 -0500

    Added message

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Page = () => {
-  return <div className="text-9xl"></div>;
+  return (
+    <div className="text-3xl">
+      Log in with Spotify to see the song you're currently listening to.
+    </div>
+  );
 };
 
 export default Page;
