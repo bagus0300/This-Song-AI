2eb5646302f97a0f66115f70b050193b55cc33f9
marvin9830 <asamieto965@gmail.com>
2023-11-20 16:11:12 -0500

    Changed landing page

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,17 @@
-import React from "react";
+"use client";
+
+import { TokenContext } from "@/context/ContextProvider";
+import React, { useContext } from "react";
 
 const Page = () => {
+  const { token } = useContext(TokenContext);
+
+  if (token) {
+    window.location.replace("/song");
+  }
+
   return (
-    <div className="text-3xl">
+    <div className="text-base">
       Log in with Spotify to see the song you are currently listening to.
     </div>
   );
