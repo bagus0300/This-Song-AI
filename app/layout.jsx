74037fb8c6c8ba2bd644b161567234b264f2ce93
marvin9830 <asamieto965@gmail.com>
2023-12-22 17:43:23 -0500

    Fixed footer

--- app/layout.jsx
+++ app/layout.jsx
@@ -17,62 +17,13 @@ export default function RootLayout({ children }) {
     <html lang="en" suppressHydrationWarning>
       <body className={`${inter.className} antialiased overflow-x-hidden`}>
         <main className="gap-2 mx-auto">{children}</main>
-        {/* <footer className="flex flex-col items-end justify-center w-full gap-4 p-4 bg-card">
-          <p className="text-center">
-            All songs, lyrics, and images are property of their respective
-            owners.
-          </p>
-          <p>This Song &copy; 2023</p>
-        </footer> */}
         <footer class="bg-secondary">
           <div class="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
-            {/* <div class="sm:flex sm:items-center sm:justify-end">
-              <div class="w-[100px]">
-                <img src="/this_song.svg" alt="This Song logo" width={100} />
-              </div>
-
-              <ul class="mt-8 flex gap-6 sm:mt-0 justify-end">
-                <li>
-                  <a
-                    href="https://github.com/atopala7/spotify-react"
-                    rel="noreferrer"
-                    target="_blank"
-                    class="text-gray-700 transition hover:opacity-75"
-                  >
-                    <span class="sr-only">GitHub</span>
-                    <Github />
-                  </a>
-                </li>
-              </ul>
-            </div> */}
-
             <div class="text-center sm:text-end grid grid-cols-1 gap-8 pt-8 sm:grid-cols-2 lg:grid-cols-2 lg:pt-16 mb-10">
-              <div>
-                {/* <p class="font-medium text-gray-900">This Song</p> */}
-
-                {/* <ul class="mt-6 space-y-4 text-sm">
-                  <li>
-                    <a
-                      href="#"
-                      class="text-gray-700 transition hover:opacity-75"
-                    >
-                      About
-                    </a>
-                  </li>
-
-                  <li>
-                    <a
-                      href="#"
-                      class="text-gray-700 transition hover:opacity-75"
-                    >
-                      Meet the Team
-                    </a>
-                  </li>
-                </ul> */}
-              </div>
+              <div></div>
 
               <div>
-                {/* <p class="font-medium text-gray-900">Helpful Links</p> */}
+                <p class="font-medium text-gray-900">This Song</p>
 
                 <ul class="text-sm">
                   <li>
@@ -80,20 +31,18 @@ export default function RootLayout({ children }) {
                       href="mailto:atopala7@gmail.com"
                       class="text-gray-700 transition hover:opacity-75"
                     >
-                      {" "}
-                      Contact{" "}
+                      Contact
                     </a>
                   </li>
 
-                  {/* <li>
+                  <li>
                     <a
                       href="#"
                       class="text-gray-700 transition hover:opacity-75"
                     >
-                      {" "}
-                      FAQs{" "}
+                      {/* About */}
                     </a>
-                  </li> */}
+                  </li>
                 </ul>
               </div>
             </div>
