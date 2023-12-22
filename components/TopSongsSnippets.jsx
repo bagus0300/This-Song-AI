81c299db9bc02c8a33f2998c5c28165452fa33ca
marvin9830 <asamieto965@gmail.com>
2023-12-22 16:52:45 -0500

    Changed text alignment

--- components/TopSongsSnippets.jsx
+++ components/TopSongsSnippets.jsx
@@ -9,76 +9,84 @@ const BACKEND_URI =
 const GPT_SUMMARY_ENDPOINT = `${BACKEND_URI}/summary`;
 
 const TopSongsSnippets = async () => {
-  const { data } = await axios.get(`${BACKEND_URI}/client_token`);
-
-  const token = data.access_token;
-
-  console.log("Getting top songs...");
-  axios.defaults.baseURL = "https://api.spotify.com/v1";
-  axios.defaults.headers["Content-Type"] = "application/json";
-  const topSongs = await axios.get(
-    `https://api.spotify.com/v1/playlists/37i9dQZEVXbMDoHDwVN2tF/tracks?limit=20`,
-    {
-      headers: {
-        Authorization: `Bearer ${token}`
-      }
-    }
-  );
-
-  const songs = topSongs.data.items;
-  // console.log("songs", songs);
-
+  let topSongs = null;
   const summaries = new Map();
+  let errorMessage = "";
 
-  await Promise.all(
-    songs.map(async (element) => {
-      // console.log(element.track.name);
-      const songID = element.track.id;
-      const songName = element.track.name;
+  try {
+    const { data } = await axios.get(`${BACKEND_URI}/client_token`);
 
-      const gpt4Response = await fetch(GPT_SUMMARY_ENDPOINT, {
-        method: "POST",
+    const token = data.access_token;
+
+    console.log("Getting top songs...");
+    axios.defaults.baseURL = "https://api.spotify.com/v1";
+    axios.defaults.headers["Content-Type"] = "application/json";
+    topSongs = await axios.get(
+      `https://api.spotify.com/v1/playlists/37i9dQZEVXbMDoHDwVN2tF/tracks?limit=20`,
+      {
         headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-          songID: songID,
-          trackName: songName
-        })
-      });
-
-      if (gpt4Response.ok) {
-        const summary = await gpt4Response.text();
-        if (summary) {
-          const firstLetter = summary.slice(13, 14);
-          const restOfSummary = summary.slice(14);
-          summaries.set(
-            element.track.id,
-            firstLetter.toUpperCase() + restOfSummary
-          );
+          Authorization: `Bearer ${token}`
         }
       }
-    })
-  );
+    );
+
+    const songs = topSongs.data.items;
+    // console.log("songs", songs);
+
+    await Promise.all(
+      songs.map(async (element) => {
+        // console.log(element.track.name);
+        const songID = element.track.id;
+        const songName = element.track.name;
+
+        const gpt4Response = await fetch(GPT_SUMMARY_ENDPOINT, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify({
+            songID: songID,
+            trackName: songName
+          })
+        });
+
+        if (gpt4Response.ok) {
+          const summary = await gpt4Response.text();
+          if (summary) {
+            const firstLetter = summary.slice(13, 14);
+            const restOfSummary = summary.slice(14);
+            summaries.set(
+              element.track.id,
+              firstLetter.toUpperCase() + restOfSummary
+            );
+          }
+        }
+      })
+    );
+  } catch (e) {
+    console.log(e);
+    errorMessage = JSON.stringify(e);
+    errorMessage = e.code;
+  }
 
   return (
     <section className="w-full gap-1">
       <div className="flex flex-wrap items-center justify-center w-full">
-        {topSongs &&
+        {(topSongs &&
           topSongs.data.items.map((item, index) => (
             <div
               className="flex py-4 m-4 transition-all duration-500 border-[1px] rounded-lg cursor-pointer hover:bg-secondary group md:w-[400px] w-full h-[200px] items-center justify-center"
               key={index}
             >
-              <a href={`/song/${item.track.id}`}>
-                <div className="w-full md:w-[400px] h-[200px] flex flex-col items-center justify-center">
-                  <div className="flex items-center w-full gap-2 p-2 overflow-x-hidden">
+              <a href={`/song/${item.track.id}`} className="w-full">
+                <div className="w-full md:w-[400px] h-[200px] flex flex-col justify-center p-1">
+                  <div className="flex justify-center w-full gap-4 p-2 overflow-x-hidden">
                     <img
                       className="w-16 h-16"
                       src={item.track.album.images[2].url}
                       alt="Album image"
                     />
-                    <p className="justify-end flex-1 overflow-x-hidden duration-500 whitespace-nowrap text-ellipsis">
+                    <p className="overflow-x-hidden duration-500 whitespace-nowrap text-ellipsis">
                       {item.track.name}
                       <br />
                       <span className="inline-flex justify-between text-muted">
@@ -97,7 +105,11 @@ const TopSongsSnippets = async () => {
                 </div>
               </a>
             </div>
-          ))}
+          ))) || (
+          <>
+            <p>Failed to connect to server: {errorMessage}.</p>
+          </>
+        )}
       </div>
     </section>
   );
