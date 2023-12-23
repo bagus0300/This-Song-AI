2fd8465e759842d00359a344751326902ddf7aaf
marvin9830 <asamieto965@gmail.com>
2023-12-22 19:26:55 -0500

    Changed font

--- app/page.jsx
+++ app/page.jsx
@@ -1,13 +1,15 @@
 import TopSongsSnippets from "@/components/TopSongsSnippets";
 import TopSongsSnippetsSkeleton from "@/components/TopSongsSnippetsSkeleton";
 import { Suspense } from "react";
+import { rajdhani } from "@/components/ui/fonts";
+import clsx from "clsx";
 
 const Page = () => {
   return (
     <>
       <div className="flex flex-col items-center justify-center gap-5 p-10 text-base text-center align-bottom">
         <img src="/this_song.svg" alt="This Song logo" width={200} />
-        <h1 className="text-xl">
+        <h1 className={clsx(rajdhani.className, `text-2xl`)}>
           Discover the meaning behind your favorite songs through AI-enhanced
           lyric analysis!
         </h1>
@@ -18,24 +20,33 @@ const Page = () => {
           Click here to search for a song!
         </a>
         <div>
-          <h2 className="px-2 mt-2 text-lg text-center md:mt-10">
+          <h2
+            className={clsx(
+              rajdhani.className,
+              "px-2 mt-2 text-lg text-center md:mt-10"
+            )}
+          >
             Or check out what GPT-4 has to say about the songs everybody&apos;s
             talking about:
           </h2>
-          <p className="text-sm">(Select a song to learn more)</p>
+          <p className={clsx(rajdhani.className, "text-base")}>
+            (Select a song to learn more)
+          </p>
           <Suspense fallback={<TopSongsSnippetsSkeleton />}>
             <TopSongsSnippets />
           </Suspense>
         </div>
       </div>
-      <footer className="bg-secondary">
+      <footer className={clsx(rajdhani.className, "bg-secondary")}>
         <div className="max-w-screen-xl px-4 py-4 mx-auto sm:px-6 lg:px-8">
           <div className="grid grid-cols-1 gap-8 pt-8 mb-10 text-center sm:text-end sm:grid-cols-2 lg:grid-cols-2">
             <div></div>
             <div>
-              <p className="font-medium text-gray-900">This Song</p>
+              <p className="text-lg font-semibold tracking-widest text-gray-900">
+                This Song
+              </p>
 
-              <ul className="text-sm">
+              <ul className="text-base">
                 <li>
                   <a
                     href="mailto:atopala7@gmail.com"
@@ -58,11 +69,11 @@ const Page = () => {
           </div>
 
           <div className="text-end">
-            <p className="text-xs text-muted">
+            <p className="text-sm text-muted">
               All songs, lyrics, and images are property of their respective
               owners.
             </p>
-            <p className="text-xs text-muted">
+            <p className="text-sm text-muted">
               &copy; 2023. This Song. All rights reserved.
             </p>
           </div>
