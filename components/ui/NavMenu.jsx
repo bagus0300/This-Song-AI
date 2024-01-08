37c2f3aea936f4b15d1bbaa06ee5f50d5afcdfdc
marvin9830 <asamieto965@gmail.com>
2024-01-08 17:10:30 -0500

    Added underline for hovered nav links

--- components/ui/NavMenu.jsx
+++ components/ui/NavMenu.jsx
@@ -12,7 +12,8 @@ import { rajdhani } from "@/components/ui/fonts";
 import ProfileButton from "./ProfileButton";
 
 const ACTIVE_ROUTE = "py-1 px-2 rounded-md text-green-500";
-const INACTIVE_ROUTE = "py-1 px-2 rounded-md bg-transparent text-foreground";
+const INACTIVE_ROUTE =
+  "py-1 px-2 rounded-md bg-transparent text-foreground hover:underline";
 
 const AuthButton = () => {
   const { data: session } = useSession();
