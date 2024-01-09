00381b71fe0cbbe000e145b8056f596c046a0392
marvin9830 <asamieto965@gmail.com>
2024-01-09 15:23:26 -0500

    Let's try to cache the playlists

--- app/song/playlist/page.jsx
+++ app/song/playlist/page.jsx
@@ -57,8 +57,8 @@ const PlaylistPage = () => {
             {
               headers: {
                 Authorization: `Bearer ${token}`
-              },
-              cache: "no-store"
+              }
+              // cache: "no-store"
             }
           );
           // console.log("playlistResponse", playlistResponse);
