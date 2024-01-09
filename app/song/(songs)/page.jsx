23234402ce014dc003ed07dbf25cf3e4a398b796
marvin9830 <asamieto965@gmail.com>
2024-01-08 19:12:28 -0500

    Changed page

--- app/song/(songs)/page.jsx
+++ app/song/(songs)/page.jsx
@@ -10,12 +10,8 @@ const Song = () => {
         "flex flex-col items-center justify-center pt-2"
       )}
     >
-      <h1 className="text-2xl font-bold">Welcome to This Song!</h1>
       <p className="mt-2 text-lg">
-        This Song is a tool providing AI-enhanced analysis of song lyrics.
-      </p>
-      <p className="mt-2 text-base">
-        Search for a song, or use the menu to explore popular tracks.
+        Search for a song or use the menu to explore popular tracks.
       </p>
     </section>
   );
