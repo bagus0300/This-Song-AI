37fc9e52b69027671e584ac003dbaa3dfa622cea
marvin9830 <asamieto965@gmail.com>
2024-01-10 23:17:42 -0500

    Added new playlists

--- lib/data.jsx
+++ lib/data.jsx
@@ -2,9 +2,14 @@ const playlistIDs = [
   "37i9dQZF1DWXRqgorJj26U", // Rock Classics
   "37i9dQZF1DXcBWIGoYBM5M", // Today's Top Hits
   "37i9dQZEVXbMDoHDwVN2tF", // Top 50 - Global
-  "37i9dQZF1DX0XUsuxWHRQd", // RapCaviar
   // "37i9dQZF1DX4JAvHpjipBk", // New Music Friday
+  "37i9dQZF1DX4o1oenSJRJd", // All Out 00s
+  "37i9dQZF1DXbTxeAdrVG2l", // All Out 90s
   "37i9dQZF1DX4UtSsGT1Sbe", // All Out 80s
+  "37i9dQZF1DWTJ7xPn4vNaz", // All Out 70s
+  "37i9dQZF1DXaKIA8E7WcJj", // All Out 60s
+  "37i9dQZF1DWSV3Tk4GO2fq", // All Out 50s
+  "37i9dQZF1DX0XUsuxWHRQd", // RapCaviar
   "37i9dQZF1DX8C9xQcOrE6T" // Disney Hits
 ];
 
