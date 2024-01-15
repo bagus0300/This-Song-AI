aac01a6b288a990eeb8a6bf394472fda652fac4f
marvin9830 <asamieto965@gmail.com>
2024-01-15 10:41:28 -0500

    Changed loading page

--- app/song/loading.jsx
+++ app/song/loading.jsx
@@ -4,17 +4,24 @@ import React from "react";
 const Loading = () => {
   return (
     <>
-      <div className="flex flex-col items-center justify-center w-full gap-1 align-middle md:flex-row md:gap-5">
-        {/* <Skeleton className="w-[300px] h-[300px]" />
-      <div className="flex flex-col items-center justify-center gap-1">
-        <Skeleton className="w-[400px] h-[36px]" />
-        <Skeleton className="w-[200px] h-[32px]" />
-        <Skeleton className="w-[250px] h-[28px]" />
-      </div> */}
-      </div>
-      <div className="flex flex-col items-center justify-center gap-5 text-base text-center align-bottom">
-        <p className="relative top-[56px]">Loading...</p>
-      </div>
+      <section className="w-full gap-1">
+        <div className="flex flex-wrap items-center justify-center w-full my-[10px]">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="40"
+            height="40"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="#1DB954"
+            strokeWidth="3"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className={"animate-spin"}
+          >
+            <path d="M21 12a9 9 0 1 1-6.219-8.56" />
+          </svg>
+        </div>
+      </section>
     </>
   );
 };
