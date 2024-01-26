f118f575f5689ec3e9cb2f71b6c863e7a1c1c6ba
marvin9830 <asamieto965@gmail.com>
2024-01-26 15:44:33 -0500

    Changed loading screen

--- app/(main)/playlists/layout.jsx
+++ app/(main)/playlists/layout.jsx
@@ -62,7 +62,8 @@ const PlaylistPage = ({ children }) => {
   }, []);
 
   return (
-    (ready && playlists && (
+    ready &&
+    playlists && (
       <>
         <div className="max-w-[1680px] mx-auto pb-8 md:pt-5 pt-2 flex flex-col items-center">
           <Carousel
@@ -140,41 +141,6 @@ const PlaylistPage = ({ children }) => {
           </Suspense>
         </div>
       </>
-    )) || (
-      <div className="max-w-[1680px] mx-auto pb-8 md:pt-5 pt-2 flex flex-col items-center">
-        <Carousel
-          opts={{
-            align: "start"
-          }}
-          className="w-[70vw] max-w-xl md:mx-auto"
-        >
-          <CarouselContent>
-            {Array.from({ length: 5 }).map((_, index) => (
-              <CarouselItem
-                key={index}
-                className="basis-1/2 md:basis-1/3 lg:basis-1/4 xl:basis-1/5"
-              >
-                <div className="p-1">
-                  <Card className="bg-card hover:brightness-125">
-                    <CardContent className="flex items-center justify-center p-0 overflow-hidden text-center rounded-lg cursor-pointer aspect-square">
-                      <Skeleton className="w-full h-full" />
-                      {/* <span className="absolute text-lg font-semibold">
-                        Loading...
-                      </span> */}
-                    </CardContent>
-                  </Card>
-                </div>
-              </CarouselItem>
-            ))}
-          </CarouselContent>
-          <CarouselPrevious />
-          <CarouselNext />
-        </Carousel>
-
-        {/* <Suspense fallback={<TopSongsSnippetsSkeleton />}> */}
-        {/* <Playlist playlist={selectedPlaylist} limit="20" offset="0" /> */}
-        {/* </Suspense> */}
-      </div>
     )
   );
 };
--- components/TopSongsSnippets.jsx
+++ components/TopSongsSnippets.jsx
@@ -123,7 +123,7 @@ const TopSongsSnippets = async ({ limit = 10, offset = 0 }) => {
           }}
         >
           <a
-            href="song/playlist"
+            href="/playlists"
             className="inline-block font-bold text-lg md:text-xl px-6 py-2 text-white bg-[#1DB954] rounded-full hover:brightness-110"
           >
             Explore more playlists
