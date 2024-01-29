1909f520caf004af6eb37459e5bd49f1c66a3b89
marvin9830 <asamieto965@gmail.com>
2024-01-29 10:30:19 -0500

    Removed Google code

--- app/layout.jsx
+++ app/layout.jsx
@@ -15,11 +15,11 @@ export default function RootLayout({ children }) {
     // <html lang="en">
     <html lang="en" suppressHydrationWarning>
       <head>
-        <script
+        {/* <script
           async
           src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_GOOGLE_ADSENSE}`}
           crossOrigin="anonymous"
-        ></script>
+        ></script> */}
       </head>
       <body className={`${inter.className} antialiased overflow-x-hidden`}>
         <main className="gap-2 mx-auto">{children}</main>
