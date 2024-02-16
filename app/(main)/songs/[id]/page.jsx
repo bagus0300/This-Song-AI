0f78607591907eb7d480772951fe7b1a15c0ca99
marvin9830 <asamieto965@gmail.com>
2024-02-15 22:42:33 -0500

    Remove commented out code for album art and refresh icon

--- app/(main)/songs/[id]/page.jsx
+++ app/(main)/songs/[id]/page.jsx
@@ -186,23 +186,6 @@ const Page = ({ params }) => {
                     height: scrollHeight
                   }}
                 >
-                  {/* <Image
-                    className="absolute transition-all duration-500 opacity-100 md:group-hover:opacity-50 md:group-hover:rounded-[50%] md:group-hover:brightness-50 -z-10 h-full w-full"
-                    src={song.album.images[1].url}
-                    width={300}
-                    height={300}
-                    placeholder={`data:image/svg+xml;base64,${toBase64(
-                      shimmer(300, 300)
-                    )}`}
-                    alt="Album art"
-                  />
-                  <Image
-                    className="hidden md:block absolute [transition:opacity_0.5s,transform_1s] origin-center scale-75 rotate-0 opacity-0 group-hover:opacity-75 group-hover:rotate-[360deg] hover:opacity-100 h-full w-full z-10"
-                    src="/images/refresh.png"
-                    alt="Refresh icon"
-                    width={300}
-                    height={300}
-                  /> */}
                   <img
                     className="absolute w-full h-full transition-all duration-500 opacity-100 -z-10"
                     src={song.album.images[1].url}
