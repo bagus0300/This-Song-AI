8f1efbaf8eeb59582334e4b3d47cd970af2ae24f
marvin9830 <asamieto965@gmail.com>
2024-02-22 16:11:45 -0500

    Refactor token generation function

--- app/api/token/route.js
+++ app/api/token/route.js
@@ -3,16 +3,6 @@ import { NextResponse } from "next/server";
 const CLIENT_ID = process.env.SPOTIFY_CLIENT_ID;
 const CLIENT_SECRET = process.env.SPOTIFY_CLIENT_SECRET;
 
-const generateRandomString = (length) => {
-  let text = "";
-  const possible =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  for (let i = 0; i < length; i++) {
-    text += possible.charAt(Math.floor(Math.random() * possible.length));
-  }
-  return text;
-};
-
 export async function GET(request, { params }) {
   console.log("Token request received.");
   console.log("CLIENT_ID", CLIENT_ID);
